a92a27f1c27ab31ea04d62b8c47f32d336d73ac3
Derek <xiaohuanlin1993@gmail.com>
2020-08-05 22:08:17 +0800

    update 20200805

--- Leetcode/399. Evaluate Division.cpp
+++ Leetcode/399. Evaluate Division.cpp
@@ -31,15 +31,92 @@ using namespace std;
 // The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
  
 class Solution {
+    struct ratio {
+        double num;
+        double den;
+        ratio(double _num, double _den): num(_num), den(_den) {};
+        ratio operator*(ratio &a) {
+            return move(ratio(num * a.num, den * a.den));
+        }
+    };
+
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, vector<pair<string, double>>> adjacents;
+        unordered_map<string, vector<pair<string, ratio>>> adjacents;
         for (int i = 0; i < equations.size(); i++) {
             auto &item = equations[i];
             double value = values[i];
-            if (item[0] > item[1]) {
+
+            if (adjacents.find(item[1]) == adjacents.end()) {
+                adjacents[item[1]] = vector<pair<string, ratio>>();
             }
+            adjacents[item[1]].push_back(make_pair(item[0], ratio(1, value)));
+
+            if (adjacents.find(item[0]) == adjacents.end()) {
+                adjacents[item[0]] = vector<pair<string, ratio>>();
+            }
+            adjacents[item[0]].push_back(make_pair(item[1], ratio(value, 1)));
         }
+
+        vector<ratio> path;
+        vector<double> res;
+        unordered_set<string> choices;
+        for (int j = 0; j < queries.size(); j++) {
+            double answer;
+            if (queries[j][0] > queries[j][1]) {
+                if (adjacents.find(queries[j][1]) == adjacents.end()) {
+                    res.push_back(-1);
+                } else if (find_res(answer, adjacents, path, choices, queries[j][1], queries[j][0])) {
+                    res.push_back(1 / answer);
+                } else {
+                    res.push_back(-1);
+                };
+            } else {
+                if (adjacents.find(queries[j][0]) == adjacents.end()) {
+                    res.push_back(-1);
+                } else if (find_res(answer, adjacents, path, choices, queries[j][0], queries[j][1])) {
+                    res.push_back(answer);
+                } else {
+                    res.push_back(-1);
+                }
+            }
+            path.clear();
+            choices.clear();
+        }
+        return move(res);
+    }
+    
+    bool find_res(double &res, unordered_map<string, vector<pair<string, ratio>>> &adjacents,
+            vector<ratio> &path, unordered_set<string> &choices, string &start, string &end)
+    {
+        if (start == end) {
+            // get the result
+            ratio t(1, 1);
+            for (int i = 0; i < path.size(); i++) {
+                t = t * path[i];
+            }
+            res = t.num / t.den;
+            return true;    
+        }
+
+        res = -1;
+        if (adjacents.find(start) == adjacents.end()) {
+            return false;
+        }
+        auto &all_things = adjacents[start];
+        for (int i = 0; i < all_things.size(); i++) {
+            if (choices.find(all_things[i].first) != choices.end()) {
+                continue;
+            }
+            path.push_back(all_things[i].second);
+            choices.insert(all_things[i].first);
+            if (find_res(res, adjacents, path, choices, all_things[i].first, end)) {
+                return true;
+            }
+            choices.erase(all_things[i].first);
+            path.pop_back();
+        }
+        return false;
     }
 };
 
@@ -48,7 +125,7 @@ int main() {
     vector<vector<string>> array = {{"a", "b"}, {"b", "c"}};
     vector<double> value = {2.0, 3.0};
     vector<vector<string>> query = {
-        {"a", "c"}, {"b", "a"}, {"a", "e"}, {"a", "a"}, {"x", "x"}
+        {"x", "x"}
     };
     auto res = s.calcEquation(array, value, query);
 

3e441001b74f597ec6cf11a5f591f8665e336886
Derek <xiaohuanlin1993@gmail.com>
2020-08-26 21:42:59 +0800

    update 20200826

---
+++ Leetcode/470. Implement Rand10() Using Rand7().cpp
@@ -0,0 +1,64 @@
+#include <unordered_set>
+#include <unordered_map>
+#include <math.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+// Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10.
+
+// Do NOT use system's Math.random().
+
+ 
+
+// Example 1:
+
+// Input: 1
+// Output: [7]
+// Example 2:
+
+// Input: 2
+// Output: [8,4]
+// Example 3:
+
+// Input: 3
+// Output: [8,1,10]
+ 
+
+// Note:
+
+// rand7 is predefined.
+// Each testcase has one argument: n, the number of times that rand10 is called.
+ 
+
+// Follow up:
+
+// What is the expected value for the number of calls to rand7() function?
+// Could you minimize the number of calls to rand7()?
+
+
+// The rand7() API is already defined for you.
+int rand7();
+
+class Solution {
+public:
+    int rand10() {
+        int result = 40;
+        while (result >= 40) {
+            result = 7 * (rand7() - 1) + rand7() - 1;
+        }
+        return result % 10 + 1;
+    }
+};
+
+
+int main() {
+    Solution s;
+    cout << s.rand10() << endl;
+}

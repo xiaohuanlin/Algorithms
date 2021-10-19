4e4a292cfefd35352ac7d6924ca57648d08613f4
xiaohuanlin <xiaohuanlin1993@gmail.com>
2021-10-19 09:39:37 -0400

    20211019

---
+++ Leetcode/969. Pancake Sorting.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+// Given an array of integers arr, sort the array by performing a series of pancake flips.
+
+// In one pancake flip we do the following steps:
+
+// Choose an integer k where 1 <= k <= arr.length.
+// Reverse the sub-array arr[0...k-1] (0-indexed).
+// For example, if arr = [3,2,1,4] and we performed a pancake flip choosing k = 3, we reverse the sub-array [3,2,1], so arr = [1,2,3,4] after the pancake flip at k = 3.
+
+// Return an array of the k-values corresponding to a sequence of pancake flips that sort arr. Any valid answer that sorts the array within 10 * arr.length flips will be judged as correct.
+
+ 
+
+// Example 1:
+
+// Input: arr = [3,2,4,1]
+// Output: [4,2,4,3]
+// Explanation: 
+// We perform 4 pancake flips, with k values 4, 2, 4, and 3.
+// Starting state: arr = [3, 2, 4, 1]
+// After 1st flip (k = 4): arr = [1, 4, 2, 3]
+// After 2nd flip (k = 2): arr = [4, 1, 2, 3]
+// After 3rd flip (k = 4): arr = [3, 2, 1, 4]
+// After 4th flip (k = 3): arr = [1, 2, 3, 4], which is sorted.
+// Example 2:
+
+// Input: arr = [1,2,3]
+// Output: []
+// Explanation: The input is already sorted, so there is no need to flip anything.
+// Note that other answers, such as [3, 3], would also be accepted.
+ 
+
+// Constraints:
+
+// 1 <= arr.length <= 100
+// 1 <= arr[i] <= arr.length
+// All integers in arr are unique (i.e. arr is a permutation of the integers from 1 to arr.length).
+
+class Solution969 {
+    public List<Integer> pancakeSort(int[] arr) {
+        List<Integer> res = new ArrayList<>();
+        iter(arr, arr.length, res);
+        return res;
+    }
+
+    public void iter(int[] arr, int end, List<Integer> res) {
+        if (end == 1) {
+            return;
+        }
+
+        int maxIndex = 0;
+        int maxValue = arr[0];
+        for (int i = 0; i < end; i++) {
+            if (maxValue < arr[i]) {
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        for (int left = 0, right = maxIndex; left < right; left++, right--) {
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+        }
+        res.add(maxIndex + 1);
+
+        for (int left = 0, right = end - 1; left < right; left++, right--) {
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+        }
+        res.add(end);
+
+        iter(arr, end - 1, res);
+    }
+}
+
+class Driver969 {
+    public static void main(String[] args) {
+        Solution969 solution = new Solution969();
+
+        int[] nums = {3,2,4,1};
+        System.out.println(solution.pancakeSort(nums));
+    }
+}

2b62205d7597a414edb8f64e0e64e20873b614f1
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-02-13 09:23:16 -0500

    20220213

---
+++ Leetcode/688. Knight Probability in Chessboard.py
@@ -0,0 +1,82 @@
+'''
+On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
+
+A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
+
+
+Each time the knight is to move, it chooses one of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there.
+
+The knight continues moving until it has made exactly k moves or has moved off the chessboard.
+
+Return the probability that the knight remains on the board after it has stopped moving.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 2, row = 0, column = 0
+Output: 0.06250
+Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
+From each of those positions, there are also two moves that will keep the knight on the board.
+The total probability the knight stays on the board is 0.0625.
+Example 2:
+
+Input: n = 1, k = 0, row = 0, column = 0
+Output: 1.00000
+ 
+
+Constraints:
+
+1 <= n <= 25
+0 <= k <= 100
+0 <= row, column <= n
+'''
+
+from typing import *
+
+import unittest
+
+
+
+class Solution:
+
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        return self.helper(n, k, row, column, {})
+
+    def helper(self, n: int, k: int, row: int, column: int, mem) -> float:
+        if row < 0 or row >= n or column < 0 or column >= n:
+            return 0
+        if k == 0:
+            return 1
+        
+        key = (k, row, column)
+        if key in mem:
+            return mem[key]
+
+        prob = 0
+        directions = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2)]
+        for x_del, y_del in directions:
+            prob += self.helper(n, k - 1, row + x_del, column + y_del, mem) / 8
+        
+        mem[key] = prob
+        return prob
+
+
+    
+class TestSolution(unittest.TestCase):
+    def test_case(self):
+        examples = (
+            ((3, 1, 0, 0), 0.25),
+            ((3, 2, 0, 0), 0.0625),
+            ((1, 0, 0, 0), 1),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().knightProbability(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()
+

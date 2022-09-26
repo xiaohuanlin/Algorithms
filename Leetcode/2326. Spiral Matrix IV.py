f4d09f9fca3c9b1521a279a30c7a353fab755a01
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-09-26 09:30:39 -0400

    20220926

---
+++ Leetcode/2326. Spiral Matrix IV.py
@@ -0,0 +1,77 @@
+'''
+You are given two integers m and n, which represent the dimensions of a matrix.
+
+You are also given the head of a linked list of integers.
+
+Generate an m x n matrix that contains the integers in the linked list presented in spiral order (clockwise), starting from the top-left of the matrix. If there are remaining empty spaces, fill them with -1.
+
+Return the generated matrix.
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 5, head = [3,0,2,6,8,1,7,9,4,2,5,5,0]
+Output: [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]]
+Explanation: The diagram above shows how the values are printed in the matrix.
+Note that the remaining spaces in the matrix are filled with -1.
+Example 2:
+
+
+Input: m = 1, n = 4, head = [0,1,2]
+Output: [[0,1,2,-1]]
+Explanation: The diagram above shows how the values are printed from left to right in the matrix.
+The last space in the matrix is set to -1.
+ 
+
+Constraints:
+
+1 <= m, n <= 105
+1 <= m * n <= 105
+The number of nodes in the list is in the range [1, m * n].
+0 <= Node.val <= 1000
+'''
+
+from typing import *
+
+import unittest
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        matrix = [[-1 for _ in range(n)] for _ in range(m)]
+        x = 0
+        y = 0
+        direct = (0, 1)
+        direction_map = {
+            (0, 1): (1, 0),
+            (1, 0): (0, -1),
+            (0, -1): (-1, 0),
+            (-1, 0): (0, 1)
+        }
+        
+        while head:
+            matrix[x][y] = head.val
+            next_x = x + direct[0]
+            next_y = y + direct[1]
+            if next_x >= 0 and next_x < m and next_y >= 0 and next_y < n and matrix[next_x][next_y] == -1:
+                pass
+            else:
+                # change direction
+                direct = direction_map[direct]
+                next_x = x + direct[0]
+                next_y = y + direct[1]
+                
+            x = next_x
+            y = next_y
+            
+            head = head.next
+        return matrix
+

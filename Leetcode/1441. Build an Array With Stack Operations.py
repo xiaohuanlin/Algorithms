e75431f1a5e35e9683ec196baa1d424cef4e5dde
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-11-12 07:16:30 -0500

    20221112

---
+++ Leetcode/1441. Build an Array With Stack Operations.py
@@ -0,0 +1,88 @@
+'''
+You are given an integer array target and an integer n.
+
+You have an empty stack with the two following operations:
+
+"Push": pushes an integer to the top of the stack.
+"Pop": removes the integer on the top of the stack.
+You also have a stream of the integers in the range [1, n].
+
+Use the two stack operations to make the numbers in the stack (from the bottom to the top) equal to target. You should follow the following rules:
+
+If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
+If the stack is not empty, pop the integer at the top of the stack.
+If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
+Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them.
+
+ 
+
+Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Pop the integer on the top of the stack. s = [1].
+Read 3 from the stream and push it to the stack. s = [1,3].
+Example 2:
+
+Input: target = [1,2,3], n = 3
+Output: ["Push","Push","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Read 3 from the stream and push it to the stack. s = [1,2,3].
+Example 3:
+
+Input: target = [1,2], n = 4
+Output: ["Push","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Since the stack (from the bottom to the top) is equal to target, we stop the stack operations.
+The answers that read integer 3 from the stream are not accepted.
+ 
+
+Constraints:
+
+1 <= target.length <= 100
+1 <= n <= 100
+1 <= target[i] <= n
+target is strictly increasing.
+'''
+
+from typing import *
+import unittest
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        index = 0
+        for i in range(1, n+1):
+            res.append("Push")
+            if i == target[index]:
+                index += 1
+            else:
+                res.append("Pop")
+
+            if index == len(target):
+                break
+        return res
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([1,2,3],3),["Push","Push","Push"]),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().buildArray(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

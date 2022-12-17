32c98cd4c7aef7a19cbcbda5cc65b64757d68727
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-12-17 08:24:08 -0500

    20221217

---
+++ Leetcode/2487. Remove Nodes From Linked List.py
@@ -0,0 +1,54 @@
+'''
+You are given the head of a linked list.
+
+Remove every node which has a node with a strictly greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [5,2,13,3,8]
+Output: [13,8]
+Explanation: The nodes that should be removed are 5, 2 and 3.
+- Node 13 is to the right of node 5.
+- Node 13 is to the right of node 2.
+- Node 8 is to the right of node 3.
+Example 2:
+
+Input: head = [1,1,1,1]
+Output: [1,1,1,1]
+Explanation: Every node has value 1, so no nodes are removed.
+ 
+
+Constraints:
+
+The number of the nodes in the given list is in the range [1, 105].
+1 <= Node.val <= 105
+'''
+from typing import *
+
+import unittest
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+
+        stack = [dummy]
+        while head:
+            while stack and stack[-1].val < head.val and stack[-1].val != 0:
+                stack.pop()
+            stack[-1].next = head
+            stack.append(head)
+            head = head.next
+        return dummy.next
+

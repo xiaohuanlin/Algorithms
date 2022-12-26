3861f4951b9fd1a0e61a36f10ee5bee5afd76ae9
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-12-26 18:17:58 -0500

    20221226

---
+++ Leetcode/2420. Find All Good Indices.py
@@ -0,0 +1,75 @@
+'''
+You are given a 0-indexed integer array nums of size n and a positive integer k.
+
+We call an index i in the range k <= i < n - k good if the following conditions are satisfied:
+
+The k elements that are just before the index i are in non-increasing order.
+The k elements that are just after the index i are in non-decreasing order.
+Return an array of all good indices sorted in increasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,1,1,3,4,1], k = 2
+Output: [2,3]
+Explanation: There are two good indices in the array:
+- Index 2. The subarray [2,1] is in non-increasing order, and the subarray [1,3] is in non-decreasing order.
+- Index 3. The subarray [1,1] is in non-increasing order, and the subarray [3,4] is in non-decreasing order.
+Note that the index 4 is not good because [4,1] is not non-decreasing.
+Example 2:
+
+Input: nums = [2,1,1,2], k = 2
+Output: []
+Explanation: There are no good indices in this array.
+ 
+
+Constraints:
+
+n == nums.length
+3 <= n <= 105
+1 <= nums[i] <= 106
+1 <= k <= n / 2
+'''
+import unittest
+
+from typing import *
+
+class Solution:
+    def goodIndices(self, nums: List[int], k: int) -> List[int]:
+        pre = [0 for _ in range(len(nums))]
+        suf = [0 for _ in range(len(nums))]
+
+        for i in range(len(nums) - 1):
+            if i == 0 or nums[i] <= nums[i-1]:
+                pre[i+1] = pre[i] + 1
+            else:
+                pre[i+1] = 1
+        
+        for i in range(1, len(nums))[::-1]:
+            if i == len(nums) - 1 or nums[i] <= nums[i+1]:
+                suf[i-1] = suf[i] + 1
+            else:
+                suf[i-1] = 1
+        res = []
+        for i in range(len(nums)):
+            if pre[i] >= k and suf[i] >= k:
+                res.append(i)
+        return res
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([2,1,1,1,3,4,1], 2), [2,3]),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().goodIndices(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

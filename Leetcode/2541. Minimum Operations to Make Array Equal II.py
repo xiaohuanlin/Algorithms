54bcd60f501b3319368fef596f1fb893ba733b62
xiaohuanlin <123971348@qq.com>
2023-01-26 14:15:32 -0500

    20230126

---
+++ Leetcode/2541. Minimum Operations to Make Array Equal II.py
@@ -0,0 +1,66 @@
+'''
+You are given two integer arrays nums1 and nums2 of equal length n and an integer k. You can perform the following operation on nums1:
+
+Choose two indexes i and j and increment nums1[i] by k and decrement nums1[j] by k. In other words, nums1[i] = nums1[i] + k and nums1[j] = nums1[j] - k.
+nums1 is said to be equal to nums2 if for all indices i such that 0 <= i < n, nums1[i] == nums2[i].
+
+Return the minimum number of operations required to make nums1 equal to nums2. If it is impossible to make them equal, return -1.
+
+
+
+Example 1:
+
+Input: nums1 = [4,3,1,4], nums2 = [1,3,7,1], k = 3
+Output: 2
+Explanation: In 2 operations, we can transform nums1 to nums2.
+1st operation: i = 2, j = 0. After applying the operation, nums1 = [1,3,4,4].
+2nd operation: i = 2, j = 3. After applying the operation, nums1 = [1,3,7,1].
+One can prove that it is impossible to make arrays equal in fewer operations.
+Example 2:
+
+Input: nums1 = [3,8,5,2], nums2 = [2,4,1,6], k = 1
+Output: -1
+Explanation: It can be proved that it is impossible to make the two arrays equal.
+
+
+Constraints:
+
+n == nums1.length == nums2.length
+2 <= n <= 105
+0 <= nums1[i], nums2[j] <= 109
+0 <= k <= 105
+'''
+import unittest
+
+from typing import *
+
+class Solution:
+    def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        if k == 0:
+            return 0 if nums1 == nums2 else -1
+        if sum(nums1) != sum(nums2):
+            return -1
+        res = 0
+        for i in range(len(nums1)):
+            dis = abs(nums1[i] - nums2[i])
+            if dis % k != 0:
+                return -1
+            res += dis // k
+        return res // 2
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([4,3,1,4], [1,3,7,1], 3), 2),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().minOperations(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

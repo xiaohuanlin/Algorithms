373ac1f1f462986a8219f07dc41ea6741a2fa63a
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-03-10 08:45:56 -0500

    20230310

---
+++ Leetcode/2396. Strictly Palindromic Number.py
@@ -0,0 +1,64 @@
+'''
+An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation of the integer n in base b is palindromic.
+
+Given an integer n, return true if n is strictly palindromic and false otherwise.
+
+A string is palindromic if it reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: n = 9
+Output: false
+Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
+In base 3: 9 = 100 (base 3), which is not palindromic.
+Therefore, 9 is not strictly palindromic so we return false.
+Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
+Example 2:
+
+Input: n = 4
+Output: false
+Explanation: We only consider base 2: 4 = 100 (base 2), which is not palindromic.
+Therefore, we return false.
+
+ 
+
+Constraints:
+
+4 <= n <= 105
+'''
+import unittest
+
+from typing import *
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def is_palindromic(x, y):
+            v = []
+            while x:
+                v.append(x % y)
+                x = x // y
+            return v == v[::-1]
+
+        for m in range(2, n-1):
+            if not is_palindromic(n, m):
+                return False
+        return True
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((9,),False),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().isStrictlyPalindromic(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

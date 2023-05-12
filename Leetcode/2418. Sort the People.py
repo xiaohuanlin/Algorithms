bae15353ace71e0ab55cce9321965fc6cf8e38b6
xiaohuanlin <123971348@qq.com>
2023-05-12 09:37:13 -0400

    20230512

---
+++ Leetcode/2418. Sort the People.py
@@ -0,0 +1,54 @@
+'''
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+
+Constraints:
+
+n == names.length == heights.length
+1 <= n <= 103
+1 <= names[i].length <= 20
+1 <= heights[i] <= 105
+names[i] consists of lower and upper case English letters.
+All the values of heights are distinct.
+'''
+import unittest
+
+from typing import *
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [n for _, n in sorted(list(zip(heights, names)), reverse=True)]
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((["Mary","John","Emma"],[180,165,170]),["Mary","Emma","John"]),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().sortPeople(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

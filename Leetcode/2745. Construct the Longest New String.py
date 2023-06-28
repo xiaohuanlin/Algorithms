a065f12b1a743d6f56f0628c21d0a3fb4bb7e708
xiaohuanlin <123971348@qq.com>
2023-06-28 10:02:02 -0400

    20230628

---
+++ Leetcode/2745. Construct the Longest New String.py
@@ -0,0 +1,55 @@
+'''
+You are given three integers x, y, and z.
+
+You have x strings equal to "AA", y strings equal to "BB", and z strings equal to "AB". You want to choose some (possibly all or none) of these strings and concactenate them in some order to form a new string. This new string must not contain "AAA" or "BBB" as a substring.
+
+Return the maximum possible length of the new string.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+
+
+Example 1:
+
+Input: x = 2, y = 5, z = 1
+Output: 12
+Explanation: We can concactenate the strings "BB", "AA", "BB", "AA", "BB", and "AB" in that order. Then, our new string is "BBAABBAABBAB".
+That string has length 12, and we can show that it is impossible to construct a string of longer length.
+Example 2:
+
+Input: x = 3, y = 2, z = 2
+Output: 14
+Explanation: We can concactenate the strings "AB", "AB", "AA", "BB", "AA", "BB", and "AA" in that order. Then, our new string is "ABABAABBAABBAA".
+That string has length 14, and we can show that it is impossible to construct a string of longer length.
+
+
+Constraints:
+
+1 <= x, y, z <= 50
+'''
+from typing import *
+
+import unittest
+
+class Solution:
+    def longestString(self, x: int, y: int, z: int) -> int:
+        return 2 * (min(x, y) * 2 + (1 if x != y else 0) + z)
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((9,9,34),104),
+            ((1,39,14),34),
+            ((2,5,1),12),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().longestString(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

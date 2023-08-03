3fec5a43b5bb96abe638d27150a54de551fc9ff6
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-08-03 08:52:35 -0400

    20230803

---
+++ Leetcode/2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,77 @@
+'''
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+ 
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+Example 2:
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+Example 3:
+
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+ 
+
+Constraints:
+
+1 <= operations.length <= 100
+operations[i] will be either "++X", "X++", "--X", or "X--".
+'''
+from typing import *
+
+import unittest
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        res = 0
+        for o in operations:
+            if '+' in o:
+                res += 1
+            else:
+                res -= 1
+        return res
+        
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((["--X","X++","X++"], ), 1),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().finalValueAfterOperations(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

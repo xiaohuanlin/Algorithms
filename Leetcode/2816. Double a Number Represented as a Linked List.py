f7ccde6de7ddee8d8c5668a69398969524f8f28c
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-08-14 21:24:12 -0400

    20230814

---
+++ Leetcode/2816. Double a Number Represented as a Linked List.py
@@ -0,0 +1,53 @@
+'''
+You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes.
+
+Return the head of the linked list after doubling it.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,8,9]
+Output: [3,7,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 189. Hence, the returned linked list represents the number 189 * 2 = 378.
+Example 2:
+
+
+Input: head = [9,9,9]
+Output: [1,9,9,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 999. Hence, the returned linked list reprersents the number 999 * 2 = 1998. 
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 104]
+0 <= Node.val <= 9
+The input is generated such that the list represents a number that does not have leading zeros, except the number 0 itself.
+'''
+from typing import *
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        dummy = ListNode()
+        res_cur = dummy
+        while cur:
+            cur_val = cur.val * 2
+            if cur_val >= 10:
+                res_cur.val += 1
+                cur_val -= 10
+            res_cur.next = ListNode(cur_val)
+
+            cur = cur.next
+            res_cur = res_cur.next
+        return dummy if dummy.val > 0 else dummy.next
+
+            
+        

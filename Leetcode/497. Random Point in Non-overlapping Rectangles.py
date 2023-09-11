6bfaddc321a362c3834fc672dc1bc357c2623ce2
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-09-11 09:34:48 -0400

    20230911

---
+++ Leetcode/497. Random Point in Non-overlapping Rectangles.py
@@ -0,0 +1,71 @@
+'''
+You are given an array of non-overlapping axis-aligned rectangles rects where rects[i] = [ai, bi, xi, yi] indicates that (ai, bi) is the bottom-left corner point of the ith rectangle and (xi, yi) is the top-right corner point of the ith rectangle. Design an algorithm to pick a random integer point inside the space covered by one of the given rectangles. A point on the perimeter of a rectangle is included in the space covered by the rectangle.
+
+Any integer point inside the space covered by one of the given rectangles should be equally likely to be returned.
+
+Note that an integer point is a point that has integer coordinates.
+
+Implement the Solution class:
+
+Solution(int[][] rects) Initializes the object with the given rectangles rects.
+int[] pick() Returns a random integer point [u, v] inside the space covered by one of the given rectangles.
+ 
+
+Example 1:
+
+
+Input
+["Solution", "pick", "pick", "pick", "pick", "pick"]
+[[[[-2, -2, 1, 1], [2, 2, 4, 6]]], [], [], [], [], []]
+Output
+[null, [1, -2], [1, -1], [-1, -2], [-2, -2], [0, 0]]
+
+Explanation
+Solution solution = new Solution([[-2, -2, 1, 1], [2, 2, 4, 6]]);
+solution.pick(); // return [1, -2]
+solution.pick(); // return [1, -1]
+solution.pick(); // return [-1, -2]
+solution.pick(); // return [-2, -2]
+solution.pick(); // return [0, 0]
+ 
+
+Constraints:
+
+1 <= rects.length <= 100
+rects[i].length == 4
+-109 <= ai < xi <= 109
+-109 <= bi < yi <= 109
+xi - ai <= 2000
+yi - bi <= 2000
+All the rectangles do not overlap.
+At most 104 calls will be made to pick.
+'''
+from typing import *
+
+from random import randint
+from bisect import bisect_left
+
+class Solution:
+
+    def __init__(self, rects: List[List[int]]):
+        self.rects = rects
+        self.sum = [0]
+        for a, b, x, y in rects:
+            self.sum.append((x - a + 1) * (y - b + 1) + self.sum[-1])
+
+    def pick(self) -> List[int]:
+        c = randint(1, self.sum[-1])
+        i = bisect_left(self.sum, c)
+
+        remain = c - self.sum[i-1] - 1
+        a, b, x, y = self.rects[i-1]
+        col = y - b + 1
+        delta_row = remain // col
+        delta_col = remain % col
+        return [a + delta_row, b + delta_col]
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(rects)
+# param_1 = obj.pick()

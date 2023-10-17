451ff5fd82373cdcc86f0abd02a193b2291cedca
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-10-17 09:27:57 -0400

    20231012

---
+++ Leetcode/2870. Minimum Number of Operations to Make Array Empty.py
@@ -0,0 +1,66 @@
+'''
+You are given a 0-indexed array nums consisting of positive integers.
+
+There are two types of operations that you can apply on the array any number of times:
+
+Choose two elements with equal values and delete them from the array.
+Choose three elements with equal values and delete them from the array.
+Return the minimum number of operations required to make the array empty, or -1 if it is not possible.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,3,2,2,4,2,3,4]
+Output: 4
+Explanation: We can apply the following operations to make the array empty:
+- Apply the first operation on the elements at indices 0 and 3. The resulting array is nums = [3,3,2,4,2,3,4].
+- Apply the first operation on the elements at indices 2 and 4. The resulting array is nums = [3,3,4,3,4].
+- Apply the second operation on the elements at indices 0, 1, and 3. The resulting array is nums = [4,4].
+- Apply the first operation on the elements at indices 0 and 1. The resulting array is nums = [].
+It can be shown that we cannot make the array empty in less than 4 operations.
+Example 2:
+
+Input: nums = [2,1,2,2,3,3]
+Output: -1
+Explanation: It is impossible to empty the array.
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+1 <= nums[i] <= 106
+'''
+from typing import *
+
+import unittest
+    
+from collections import Counter
+from math import ceil
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        res = 0
+        for n in c.values():
+            if n == 1:
+                return -1
+            res += ceil(n / 3)
+        return res
+        
+        
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([2,3,3,2,2,4,2,3,4],),4),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().minOperations(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

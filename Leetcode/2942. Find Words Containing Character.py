4a991ad19aa7fb6f11428e220a2295af11b4bbc1
xiaohuanlin <xiaohuanlin1993@gmail.com>
2023-11-28 09:05:17 -0500

    20231122

---
+++ Leetcode/2942. Find Words Containing Character.py
@@ -0,0 +1,58 @@
+'''
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 50
+x is a lowercase English letter.
+words[i] consists only of lowercase English letters.
+'''
+import unittest
+
+from typing import *
+
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [i for i in range(len(words)) if x in words[i]]
+    
+    
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((["abc","bcd","aaaa","cbc"],'a'),[0,2]),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().findWordsContaining(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

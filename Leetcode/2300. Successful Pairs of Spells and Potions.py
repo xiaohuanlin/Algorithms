6b09245ddd0b3c498048073e0da048af2fc86701
xiaohuanlin <123971348@qq.com>
2023-12-27 10:11:53 -0500

    20231220

---
+++ Leetcode/2300. Successful Pairs of Spells and Potions.py
@@ -0,0 +1,70 @@
+'''
+You are given two positive integer arrays spells and potions, of length n and m respectively, where spells[i] represents the strength of the ith spell and potions[j] represents the strength of the jth potion.
+
+You are also given an integer success. A spell and potion pair is considered successful if the product of their strengths is at least success.
+
+Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
+
+
+
+Example 1:
+
+Input: spells = [5,1,3], potions = [1,2,3,4,5], success = 7
+Output: [4,0,3]
+Explanation:
+- 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+- 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+- 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
+Thus, [4,0,3] is returned.
+Example 2:
+
+Input: spells = [3,1,2], potions = [8,5,8], success = 16
+Output: [2,0,2]
+Explanation:
+- 0th spell: 3 * [8,5,8] = [24,15,24]. 2 pairs are successful.
+- 1st spell: 1 * [8,5,8] = [8,5,8]. 0 pairs are successful.
+- 2nd spell: 2 * [8,5,8] = [16,10,16]. 2 pairs are successful.
+Thus, [2,0,2] is returned.
+
+
+Constraints:
+
+n == spells.length
+m == potions.length
+1 <= n, m <= 105
+1 <= spells[i], potions[i] <= 105
+1 <= success <= 1010
+'''
+
+from typing import *
+import unittest
+
+
+from bisect import bisect_left
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        res = []
+        for s in spells:
+            target = success / s
+            i = bisect_left(potions, target)
+            res.append(len(potions) - i)
+        return res
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([5,1,3],[1,2,3,4,5],7),[4,0,3]),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().successfulPairs(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

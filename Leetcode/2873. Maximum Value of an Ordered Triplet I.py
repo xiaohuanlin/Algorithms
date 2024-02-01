fd3ef1a9e9ce5775aa8a65e0ec42d85c1f33ef42
xiaohuanlin <123971348@qq.com>
2024-02-01 13:41:13 -0500

    20240130

---
+++ Leetcode/2873. Maximum Value of an Ordered Triplet I.py
@@ -0,0 +1,75 @@
+'''
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+
+
+Example 1:
+
+Input: nums = [12,6,1,2,7]
+Output: 77
+Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+It can be shown that there are no ordered triplets of indices with a value greater than 77.
+Example 2:
+
+Input: nums = [1,10,3,4,19]
+Output: 133
+Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+It can be shown that there are no ordered triplets of indices with a value greater than 133.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+
+
+Constraints:
+
+3 <= nums.length <= 100
+1 <= nums[i] <= 106
+'''
+import unittest
+
+from typing import *
+
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        left = [0 for _ in range(len(nums))]
+        left_max = 0
+        right = [0 for _ in range(len(nums))]
+        right_max = 0
+
+        for i in range(len(nums)):
+            left[i] = max(left_max - nums[i], left[i-1])
+            left_max = max(left_max, nums[i])
+
+        for i in range(len(nums))[::-1]:
+            right_max = max(right_max, nums[i])
+            right[i] = right_max
+
+        res = 0
+        for i in range(len(nums)-1):
+            res = max(left[i] * right[i+1], res)
+        return res
+
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([1,10,3,4,19],),133),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().maximumTripletValue(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

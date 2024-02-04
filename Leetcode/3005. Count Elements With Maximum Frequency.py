7633a62532ec8f4b8dca7abd4357943db6d823cd
xiaohuanlin <xiaohuanlin1993@gmail.com>
2024-02-03 21:38:23 -0500

    20240202

---
+++ Leetcode/3005. Count Elements With Maximum Frequency.py
@@ -0,0 +1,64 @@
+'''
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+'''
+import unittest
+
+from typing import *
+
+from collections import Counter
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        max_v = 0
+        res = 0
+        for k, v in c.items():
+            if v > max_v:
+                max_v = v
+                res = v
+            elif v == max_v:
+                res += v
+        return res
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            (([1,2,2,3,1,4],),4),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().maxFrequencyElements(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

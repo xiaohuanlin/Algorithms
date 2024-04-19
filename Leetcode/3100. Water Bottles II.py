86ff6fd4eeb62ba307b3bbf1cd010c45f364a4a0
xiaohuanlin <123971348@qq.com>
2024-04-19 13:48:32 -0400

    20240408

---
+++ Leetcode/3100. Water Bottles II.py
@@ -0,0 +1,64 @@
+'''
+You are given two integers numBottles and numExchange.
+
+numBottles represents the number of full water bottles that you initially have. In one operation, you can perform one of the following operations:
+
+Drink any number of full water bottles turning them into empty bottles.
+Exchange numExchange empty bottles with one full water bottle. Then, increase numExchange by one.
+Note that you cannot exchange multiple batches of empty bottles for the same value of numExchange. For example, if numBottles == 3 and numExchange == 1, you cannot exchange 3 empty water bottles for 3 full bottles.
+
+Return the maximum number of water bottles you can drink.
+
+
+
+Example 1:
+
+
+Input: numBottles = 13, numExchange = 6
+Output: 15
+Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+Example 2:
+
+
+Input: numBottles = 10, numExchange = 3
+Output: 13
+Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+
+
+Constraints:
+
+1 <= numBottles <= 100
+1 <= numExchange <= 100
+'''
+import unittest
+
+from typing import *
+
+
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        res = numBottles
+        while numBottles >= numExchange:
+            numBottles -= numExchange
+            numBottles += 1
+            numExchange += 1
+            res += 1
+        return res
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((10, 3), 13),
+            # ((13, 6), 15),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().maxBottlesDrunk(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()

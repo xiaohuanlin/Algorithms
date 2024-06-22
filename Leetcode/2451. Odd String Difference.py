0fe6d9a94197be9521da174bc30fd15b500157ad
xiaohuanlin <xiaohuanlin1993@gmail.com>
2024-06-22 07:27:24 -0400

    20240529

---
+++ Leetcode/2451. Odd String Difference.py
@@ -0,0 +1,77 @@
+'''
+You are given an array of equal-length strings words. Assume that the length of each string is n.
+
+Each string words[i] can be converted into a difference integer array difference[i] of length n - 1 where difference[i][j] = words[i][j+1] - words[i][j] where 0 <= j <= n - 2. Note that the difference between two letters is the difference between their positions in the alphabet i.e. the position of 'a' is 0, 'b' is 1, and 'z' is 25.
+
+For example, for the string "acb", the difference integer array is [2 - 0, 1 - 2] = [2, -1].
+All the strings in words have the same difference integer array, except one. You should find that string.
+
+Return the string in words that has different difference integer array.
+
+ 
+
+Example 1:
+
+Input: words = ["adc","wzy","abc"]
+Output: "abc"
+Explanation: 
+- The difference integer array of "adc" is [3 - 0, 2 - 3] = [3, -1].
+- The difference integer array of "wzy" is [25 - 22, 24 - 25]= [3, -1].
+- The difference integer array of "abc" is [1 - 0, 2 - 1] = [1, 1]. 
+The odd array out is [1, 1], so we return the corresponding string, "abc".
+Example 2:
+
+Input: words = ["aaa","bob","ccc","ddd"]
+Output: "bob"
+Explanation: All the integer arrays are [0, 0] except for "bob", which corresponds to [13, -13].
+ 
+
+Constraints:
+
+3 <= words.length <= 100
+n == words[i].length
+2 <= n <= 20
+words[i] consists of lowercase English letters.
+'''
+import unittest
+
+from typing import *
+
+from collections import defaultdict
+
+
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+        def cal(w):
+            res = []
+            for i in range(len(w)-1):
+                res.append(ord(w[i+1]) - ord(w[i]))
+            return tuple(res)
+
+        maps = defaultdict(list)
+        for i, w in enumerate(words):
+            l = cal(w)
+            maps[l].append(w)
+            if len(maps) > 1 and i >= 2:
+                for k, v in maps.items():
+                    if len(v) == 1:
+                        return v[0]
+        return ""
+        
+        
+        
+class TestSolution(unittest.TestCase):
+
+    def test_case(self):
+        examples = (
+            ((["aaa","bob","ccc","ddd"],),"bob"),
+        )
+        for first, second in examples:
+            self.assert_function(first, second)
+
+    def assert_function(self, first, second):
+        self.assertEqual(Solution().oddString(*first), second,
+                         msg="first: {}; second: {}".format(first, second))
+
+
+unittest.main()
